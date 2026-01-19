535f5e0cd0133f2b00426853ee201c838a1f2b9e
Nguyen Duy Hau <duyhau082002@gmail.com>
2026-01-19 07:19:25 +0700

    add: bootstrap html, CRUD tasks

--- src/main/java/com/app/todoapp/controller/TaskController.java
+++ src/main/java/com/app/todoapp/controller/TaskController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 //import org.springframework.web.bind.annotation.RequestMapping;
 import com.app.todoapp.service.TaskService;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -33,7 +34,17 @@ public class TaskController {
         return "redirect:/";
     }
 
+    @GetMapping("/{id}/delete")
+    public String deleteTasks(@PathVariable Long id){
+        taskService.deleteTask(id);
+        return "redirect:/";
+    }
 
+    @GetMapping("/{id}/toggle")
+    public String toggleTasks(@PathVariable Long id){
+        taskService.toggleTask(id);
+        return "redirect:/";
+    }
 
 }
 
